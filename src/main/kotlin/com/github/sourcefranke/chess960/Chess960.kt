b11904f0382591e58ec6cfc2d58810ea4880753e
sourcefranke <lukas.tietenberg@web.de>
2020-10-21 17:48:45 +0200

    refactored code structure

--- src/main/kotlin/com/github/sourcefranke/chess960/Chess960.kt
+++ src/main/kotlin/com/github/sourcefranke/chess960/Chess960.kt
@@ -1,8 +1,6 @@
 package com.github.sourcefranke.chess960
 
-enum class Piece(val symbol: Char) {
-    KING('K'), QUEEN('Q'), ROOK('R'), KNIGHT('N'), BISHOP('B')
-}
+import com.github.sourcefranke.chess960.Piece.*
 
 interface Rule {
     fun validate(pieces: List<Piece>): Boolean
@@ -12,54 +10,49 @@ interface Rule {
  * Rule: the king to be placed between the two rooks
  */
 class KingRule : Rule {
-    override fun validate(pieces: List<Piece>): Boolean {
-        return pieces.indexOf(Piece.ROOK) < pieces.indexOf(Piece.KING)
-                && pieces.indexOf(Piece.KING) < pieces.lastIndexOf(Piece.ROOK)
-    }
+    override fun validate(pieces: List<Piece>): Boolean =
+            pieces.indexOf(ROOK) < pieces.indexOf(KING)
+                    && pieces.indexOf(KING) < pieces.lastIndexOf(ROOK)
 }
 
 /**
  * Rule: the bishops to be placed on fields of different colours
  */
 class BishopRule : Rule {
-    override fun validate(pieces: List<Piece>): Boolean {
-        return pieces.indexOf(Piece.BISHOP) % 2 != pieces.lastIndexOf(Piece.BISHOP) % 2
-    }
+    override fun validate(pieces: List<Piece>): Boolean =
+            pieces.indexOf(BISHOP) % 2 != pieces.lastIndexOf(BISHOP) % 2
 }
 
 /**
  * Set of rules, where every single rule has to be fulfilled
  */
-class RuleSet (var rules: List<Rule> = listOf(BishopRule(), KingRule())) : Rule {
-    override fun validate(pieces: List<Piece>): Boolean {
-        return rules.all{ rule -> rule.validate(pieces) }
-    }
+class RuleSet (private var rules: List<Rule> = listOf(BishopRule(), KingRule())) : Rule {
+    override fun validate(pieces: List<Piece>): Boolean = rules.all{ rule -> rule.validate(pieces) }
 }
 
 /**
- * Generating a random setup based on given (set of) rules
+ * Generating a random setup based on given rules
  */
-fun generateValidSetup(rules: Rule = RuleSet()): List<Piece> {
-    var setup = listOf(Piece.ROOK, Piece.KNIGHT, Piece.BISHOP, Piece.QUEEN, Piece.KING, Piece.BISHOP, Piece.KNIGHT, Piece.ROOK)
+fun generateValidSetup(
+        rules: Rule = RuleSet(),
+        setup: List<Piece> = listOf(KING, QUEEN, ROOK, ROOK, BISHOP, BISHOP, KNIGHT, KNIGHT)
+): List<Piece> {
+    var result: List<Piece>
 
-    do {
-        setup = setup.shuffled()
-    } while (!rules.validate(setup))
+    do result = setup.shuffled()
+    while (!rules.validate(result))
 
-    return setup
+    return result
 }
 
 /**
  * Transform the given setup of pieces into its string representation
  */
-fun setupAsString(pieces: List<Piece> = generateValidSetup()): String  {
-    return pieces.map { it.symbol }.joinToString()
-}
+fun setupAsString(pieces: List<Piece> = generateValidSetup(), separator: String = ""): String =
+        pieces.map { it.symbol }.joinToString(separator = separator)
 
 /**
  * Main method:
  * Print a generated setup to the console
  */
-fun main() {
-    println(setupAsString())
-}
+fun main() = println(setupAsString(separator = " "))
---
+++ src/main/kotlin/com/github/sourcefranke/chess960/Piece.kt
@@ -0,0 +1,5 @@
+package com.github.sourcefranke.chess960
+
+enum class Piece(val symbol: Char) {
+    KING('K'), QUEEN('Q'), ROOK('R'), KNIGHT('N'), BISHOP('B')
+}
